e9dacff60436075fe77cf7b81fee6e8fbe056c05
SARAN-28 <saranponnusamy2853@gmail.com>
2026-09-25 10:51:15 +0530

    modified

--- Day6/script.js
+++ Day6/script.js
@@ -1,3 +1,106 @@
+// Timers:
+
+// 1. setTimeout():
+// setTimeout() runs a function once after a specified time.
+
+// Syntax:
+// setTimeout(function(){
+//     // code
+// }, milliseconds)
+
+// Eg:
+// setTimeout(function(){
+//     console.log("Hello Saran")
+// }, 3000)
+
+// Canceling setTimeout():
+// When setTimeout() is created, it returns an ID.
+
+// Eg:
+// let timer = setTimeout(function(){
+//     console.log("Hello Saran")
+// }, 3000)
+
+// clearTimeout(timer)
+
+// 2. setInterval()
+// setInterval() runs a function again and again at a specified interval.
+
+// Syntax:
+// setInterval(function(){
+//     // code
+// }, milliseconds)
+
+// Eg:
+// setInterval(function(){
+//     console.log("Hello")
+// }, 1000)
+
+// Canceling setInterval()
+// setInterval() also returns an ID.
+
+// let intervalId = setInterval(function(){
+//     console.log("Hello")
+// }, 1000)
+
+// clearInterval(intervalId)
+
+
+// Error Handling:
+// Error handling is used to prevent the program from crashing when an error occurs.
+
+// JavaScript provides:
+// try
+// catch
+// finally
+// throw
+
+// 1. try...catch:
+// Syntax
+// try{
+//     // code that may cause error
+// }
+// catch(error){
+//     // handle the error
+// }
+
+// Eg:
+// try{
+//     throw new Error("Custom error message")
+// }
+// catch(error){
+//     console.log("Error occurred:", error.message)
+// }
+
+// 2. finally:
+// try{
+//     console.log("Trying something...")
+//     throw new Error("Something went wrong")
+// }
+// catch(error){
+//     console.log("Caught an error")
+// }
+// finally{
+//     console.log("Finally block executed")
+// }
+
+// 3. Throwing Custom Errors:
+// Eg:
+// function checkAge(age) {
+
+//     if (age < 18) {
+//         throw new Error("Age must be 18 or above")
+//     }
+
+//     return "Valid age"
+// }
+// try {
+//     console.log(checkAge(30))
+// }
+// catch (error) {
+//     console.log(error.message)
+// }
+
 //Document Object Model
 
 // console.log(document.title)
@@ -38,7 +141,7 @@
 // let a=document.createElement("div")
 // a.textContent="<h1>Hello world</h1>"
 // a.innerText="<h1>Hello world</h1>"
-// a.innerHTML="<h1>Hello world</h1>"    
+// a.innerHTML="<h1>Hello world</h1>"
 // console.log(a);
 
 // let a=document.getElementById("head")
